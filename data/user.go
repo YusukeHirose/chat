e92e562eb3c4ce8262f23a4ea52cd2948b76c5a9
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-09-09 00:08:49 +0900

    feat: Sessionを定義

--- data/user.go
+++ data/user.go
@@ -11,8 +11,21 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    string
+	CreatedAt time.Time
+}
+
 func UserByEmail(email string) (user User, err error) {
 	user = User{}
 	// TODO emailで検索
+	return
+}
+
+func (user *User) CreateSession() (session Session, err error) {
+	// TODO DB処理
 	return
 }
