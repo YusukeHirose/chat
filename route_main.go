04e744695ee397bac04f00a270c9ff0b2d253f55
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-09-15 13:42:19 +0900

    feat: html生成をメソッド化

--- route_main.go
+++ route_main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"chat/data"
 	"net/http"
-	"text/template"
 )
 
 func index(writer http.ResponseWriter, request *http.Request) {
@@ -12,14 +11,12 @@ func index(writer http.ResponseWriter, request *http.Request) {
 	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(writer, request)
-		public_tmpl_files := []string{"templates/layout.html", "templates/public_navbar.html", "templates/index.html"}
-		private_tmpl_files := []string{"templates/layout.html", "templates/public_navbar.html", "templates/index.html"}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			// TODO navbarをpublic用にする
+			generateHTML(writer, threads, "layout", "navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			// TODO navbarをprivate用にする
+			generateHTML(writer, threads, "layout", "navbar", "index")
 		}
-		templates.ExecuteTemplate(writer, "layout", threads)
 	}
 }
