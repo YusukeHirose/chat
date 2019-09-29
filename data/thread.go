8bf7195a40a6f5309006a279976cd7ed672b73f3
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-09-30 00:23:18 +0900

    feat: Threadデータ全件取得

--- data/thread.go
+++ data/thread.go
@@ -10,6 +10,18 @@ type Thread struct {
 	CreatedAt time.Time
 }
 
-func Threads() (thread []Thread, err error) {
+func Threads() (threads []Thread, err error) {
+	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		conv := Thread{}
+		if err = rows.Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, conv)
+	}
+	rows.Close()
 	return
 }
