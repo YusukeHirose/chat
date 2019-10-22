07255c3734db7347852b0c281c5cdb4007fcd7ba
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-10-23 01:17:24 +0900

    feat: 会員登録用のDB処理実装

--- data/user.go
+++ data/user.go
@@ -34,3 +34,15 @@ func (session *Session) Check() (valid bool, err error) {
 	// TODO DB処理
 	return
 }
+
+func (user *User) Create() (err error) {
+	statement := "insert into users (uuid, name, email, password, created_at) values ($1, $2, $3, $4, $5) returning id, uuid, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(createUUID(), user.Name, user.Email, Encrypt(user.Password), time.Now()).Scan(&user.Id, &user.Uuid, &user.CreatedAt)
+	return
+}
