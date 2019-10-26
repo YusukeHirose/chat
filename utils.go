dc79d3d9ddeda77a217863a4ef08bcb6f030e01c
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-10-26 17:48:04 +0900

    feat: テンプレート作成処理を実装

--- utils.go
+++ utils.go
@@ -20,6 +20,17 @@ func session(writer http.ResponseWriter, request *http.Request) (sess data.Sessi
 	return
 }
 
+// htmlファイルを渡してテンプレートを取得する
+func parseTemplateFiles(filenames ...string) (t *template.Template) {
+	var files []string
+	t = template.New("layout")
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	t = template.Must(template.ParseFiles(files...))
+	return
+}
+
 func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
 	var files []string
 	for _, file := range filenames {
